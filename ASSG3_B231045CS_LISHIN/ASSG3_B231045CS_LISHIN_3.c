76d0473c7fb02517539257ab73de108110676850
Lishin <lishinvs6541@gmail.com>
2024-08-28 02:28:18 +0530

    #ASSG3_Q3: Double Hashing

--- ASSG3_B231045CS_LISHIN/ASSG3_B231045CS_LISHIN_3.c
+++ ASSG3_B231045CS_LISHIN/ASSG3_B231045CS_LISHIN_3.c
@@ -36,6 +36,38 @@ int QuadraticProbing(int hashTable[],int a[], int n, int m){
     return c;
 }
 
+int largestPrime(int n){
+    for(int i = n-1; i >= 2; i--){
+        int flag = 0;
+        for(int j = 2; j*j<=i; j++){
+            if(i%j == 0) flag = 1;
+        }
+        if(flag == 0) return i;
+    }
+    return 1;
+}
+
+int DoubleHashing(int hashTable[], int a[], int n, int m){
+    int c = 0;
+    int p = largestPrime(n);
+    
+    for(int i = 0; i < m; i++){
+        int hashIndex = a[i] % n;
+        int probe = 1;
+        int h1 = hashIndex;
+        int h2 = p - (a[i] % p);
+
+        while(hashTable[hashIndex]!=DELETED){
+            hashIndex = (h1 + (probe * h2)) % n;
+            probe++;
+            c++;
+        }
+        hashTable[hashIndex] = a[i];
+        printf("%d ", hashIndex);
+    }
+    return c;
+}
+
 int main(){
     int n,m;
     scanf("%d %d", &n, &m);
@@ -64,6 +96,14 @@ int main(){
                 hashTable[i] = DELETED;
             }
             collision = QuadraticProbing(hashTable, arr, n, m);
+            printf("\n");
+            printf("%d\n", collision);
+        }
+        else if(op == 'c'){
+            for(int i=0; i<n; i++){
+                hashTable[i] = DELETED;
+            }
+            collision = DoubleHashing(hashTable, arr, n, m);
             printf("\n");
             printf("%d\n", collision);
         }
