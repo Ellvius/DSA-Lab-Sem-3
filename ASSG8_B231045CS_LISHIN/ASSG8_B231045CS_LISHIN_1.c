59bcd7383f8ce0edd32d30b6e19bff660cee9b70
Lishin <lishinvs6541@gmail.com>
2024-11-04 19:13:43 +0530

    #ASSG8: Initial commit

--- ASSG8_B231045CS_LISHIN/ASSG8_B231045CS_LISHIN_1.c
+++ ASSG8_B231045CS_LISHIN/ASSG8_B231045CS_LISHIN_1.c
@@ -1,7 +1,61 @@
 #include<stdio.h>
+#include<string.h>
+
+// compute the longest prefix suffix at each index
+void computelps(char p[], int m, int lps[]){
+    int l = 0;
+    lps[0] = 0;
+    for(int i = 1; i < m; i++){
+
+        // in case the char does not match backtrack
+        while(l > 0 && p[i]!=p[l]){
+            l = lps[l-1];
+        }
+        // increase the length at each matching prefix
+        if(p[i]==p[l]){
+            l++;
+        }
+        lps[i] = l;
+    }
+}
+
+void kmp(char t[], char p[]){
+    int n = strlen(t);
+    int m = strlen(p);
+    int index = -1;
+    int f = 0;
+
+    // make the longest prefix suffix array
+    int lps[m];
+    computelps(p,m,lps);
+
+    int j = 0;
+    for(int i = 0; i < n; i++){
+
+        // in case the char does not match backtrack
+        while(j > 0 && p[j]!=t[i]){
+            j = lps[j-1];
+        }
+        // if the char are matching increase the pattern counter
+        if(p[j]==t[i]) j++;
+        // when the pattern counter equals the pattern length the pattern is present in the text
+        if(j == m) {
+            index = i - j + 1; //  length of j is actually 1 more than m since j starts from 0
+            f++;
+            j = 0;
+        }
+    }
+
+    printf("%d ", index);
+    if(f>0) printf("%d ", f);
+    printf("\n");
+}
 
 int main(){
-    
+    char t[1000000], p[100000];
+    fgets(t, 1000000, stdin);
+    fgets(p, 100000, stdin);
 
+    kmp(t, p);
     return 0;
 }
--- ASSG8_B231045CS_LISHIN/ASSG8_B231045CS_LISHIN_2.c
+++ ASSG8_B231045CS_LISHIN/ASSG8_B231045CS_LISHIN_2.c
@@ -1,7 +1,51 @@
+#include<string.h>
 #include<stdio.h>
 
-int main(){
+void badCharTable(char p[], int m, int badChar[]){
+    for(int i = 0; i < 256; i++){
+        badChar[i] = -1;
+    }
+
+    for(int i = 0; i < m; i++){
+        badChar[(int)p[i]] = i;
+    }
+}
+
+void boyerMoore(char t[], char p[]){
+    int n = strlen(t);
+    int m = strlen(p);
+
+    int badcharTable[256];
+    badCharTable(p, m, badcharTable);
+
+    int s = 0, index = -1;
     
+    while(s<=n-m){
+        int j = m-1;
+
+        for(; j >= 0 && p[j]==t[s+j]; j--){
+        }
+        // if the pattern is found 
+        int shift;
+        if(j < 0){
+            shift = (s+m < n) ? m-badcharTable[(int)t[s+m]]:1;
+            index = s;
+            printf("%d\n", index);
+            return;
+        }
+        else {
+            shift = (j-badcharTable[(int)t[s+j]] > 1) ? j-badcharTable[(int)t[s+j]]: 1;
+        }
+        s+=shift;
+    }
+}
+
+int main(){
+    char t[1000000], p[100000];
+    fgets(t, 1000000, stdin);
+    fgets(p, 100000, stdin);
+
+    boyerMoore(t, p);
 
     return 0;
 }
